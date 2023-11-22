998e4d926767626a6cfb223d304aee4d41daafe1
DeceulaerAdrien <adriendeceulaer@gmail.com>
2023-11-22 09:51:16 +0100

    refactor file secu

--- src/main/java/com/example/repositories/security/AuthRepository.java
+++ src/main/java/com/example/repositories/security/AuthRepository.java
@@ -1,12 +1,11 @@
 package com.example.repositories.security;
 
 import com.example.models.entities.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-public interface AuthRepository extends JpaRepository<Member , long> {
+public interface AuthRepository extends JpaRepository<Member,long> {
 
     @Query("Select m From Member Where m.Member_Pseudonyme Ilike :Member_Pseudonyme")
     Optional<Member> findByUserPseudo(String pseudo);
