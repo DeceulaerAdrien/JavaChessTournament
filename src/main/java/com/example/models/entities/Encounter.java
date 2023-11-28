4fb14719265f8e7ed11ee0be1ffc511771d5f3a2
BourgoinAntoine <bourgoin.antoine@hotmail.com>
2023-11-28 16:36:24 +0100

    Commit fin day

---
+++ src/main/java/com/example/models/entities/Encounter.java
@@ -0,0 +1,36 @@
+package com.example.models.entities;
+
+import com.example.models.entities.enums.EncounterResultEnum;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+@Entity
+@Getter
+@DynamicInsert
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@EntityListeners(AuditingEntityListener.class)
+public class Encounter extends BaseEntity<Long> {
+
+
+    @ManyToOne
+    private Tournament tournament;
+
+    @ManyToOne @Setter @JoinColumn(name = "White_Player_id")
+    private Member joueurBlanc;
+
+    @ManyToOne @Setter @JoinColumn(name = "Black_Player_id")
+    private Member joueurNoir;
+
+    @Setter
+    @Column(name = "Number_Round")
+    private int number_round;
+
+    @Column(name = "Result_Encounter",columnDefinition = "Varchar DEFAULT 'NOT_PLAYED'")
+    private EncounterResultEnum resultEnum;
+
+}
